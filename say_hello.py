55ba9546da17a53df787a08c02722dc2afb5428e
Goutham Sornala <goutham.sornala@winfosolutions.com>
2025-08-03 14:04:02 +0530

    Hash pw

--- say_hello.py
+++ say_hello.py
@@ -1,3 +1,15 @@
-name = 'WinfoBots'
-print(f'Hello {name}!')
-print('Hello')
+import bcrypt
+
+# Hashing a password
+password = b"MyStrongPassword"
+salt = bcrypt.gensalt()
+print(salt)
+hashed = bcrypt.hashpw(password, salt)
+print(hashed)
+
+# Verification
+user_input = b"MyStrongPassword"
+if bcrypt.checkpw(user_input, hashed):
+    print("Password matches.")
+else:
+    print("Incorrect password.")
