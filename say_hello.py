c51cdbfe4065729e981a6daa0e3dfe7f3d3aa97e
Gowtham Sornala <gowthamsornala208@gmail.com>
2025-11-27 10:42:57 +0530

    Added a function call

--- say_hello.py
+++ say_hello.py
@@ -1,16 +1,20 @@
-#v1.1.4
+#v1.1.5
 import bcrypt
 
-# Hashing a password
-password = b"MyStrongPassword"
-salt = bcrypt.gensalt()
-print(salt)
-hashed = bcrypt.hashpw(password, salt)
-print(hashed)
+def passwordHash():
+    # Hashing a password
+    password = b"MyStrongPassword"
+    salt = bcrypt.gensalt()
+    print(salt)
+    hashed = bcrypt.hashpw(password, salt)
+    return hashed
 
 # Verification
 user_input = b"MyStrongPassword"
+hashed = passwordHash(user_input)
+print(f'Hashed: {hashed}')
 if bcrypt.checkpw(user_input, hashed):
     print("Password matches.")
 else:
     print("Incorrect password.")
+
